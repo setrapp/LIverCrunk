2ba0b75e1386c8166e676567291101cb982b1b7a
Sam Trapp <>
2023-04-30 13:42:57 -0600

    Showing liver bar behind health.

--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -8,13 +8,17 @@ public class Hud : MonoBehaviour {
 	
 	public Animator anim = null;
 	public Image healthFill;
+	public Image liverFill;
 	public float minHealthPortion = 0.04f; // Going any further looks bad for spill;
 
 	public Image spillFx;
+	public Image liverSpillFx;
 	public float maxSpillPos;
 	public float minSpillPos;
 
 	private float cachedHealthPortion = 1f;
+	private float cachedLiverPortion = 1f;
+	private int cachedLiverCount = 0;
 
 	//public List<GameObject> HudLivers = null;
 	public HudLiver hudLiver = null;
@@ -34,14 +38,31 @@ public class Hud : MonoBehaviour {
 		healthPortion = Mathf.Clamp01(healthPortion);
 		healthFill.fillAmount = Mathf.Max(healthPortion, minHealthPortion);
 
+		var liverPortion = 0f;
+		if (livers.Count > 0) {
+			for (int i = 0; i < livers.Count; i++) {
+				liverPortion += livers[i].HealthPortion;
+			}
+			liverPortion = Mathf.Clamp01(liverPortion / livers.Count);
+		}
+
+		liverFill.fillAmount = Mathf.Max(liverPortion, minHealthPortion);
+
 		healthFill.enabled = healthPortion > 0;
+		liverFill.enabled = liverPortion > 0;
 
 		spillFx.enabled = healthPortion > 0 && cachedHealthPortion - healthPortion > 0;
 		cachedHealthPortion = healthPortion;
 		Vector3 spillPos = spillFx.transform.localPosition;
-		var adjustedHealthPortion = Mathf.Clamp01((healthPortion - minHealthPortion) * (1 / (1 - minHealthPortion)));
-		spillPos.x = (minSpillPos * (1 - adjustedHealthPortion)) + (maxSpillPos * adjustedHealthPortion);
+		spillPos.x = computeSpillPos(healthPortion);
 		spillFx.transform.localPosition = spillPos;
+
+		liverSpillFx.enabled = liverPortion > 0 && cachedLiverPortion - liverPortion > 0 && (cachedLiverCount == livers.Count);
+		cachedLiverPortion = liverPortion;
+		cachedLiverCount = livers.Count;
+		Vector3 liverSpillPos = liverSpillFx.transform.localPosition;
+		liverSpillPos.x = computeSpillPos(liverPortion);
+		liverSpillFx.transform.localPosition = liverSpillPos;
 
 		hudLiver.ToggleVisibility(livers.Count > 0);
 
@@ -49,5 +70,10 @@ public class Hud : MonoBehaviour {
 		if (livers.Count > 0 && livers[0] != null) {
 			hudLiver.UpdateHealth(livers[0].HealthPortion);
 		}
+	}
+
+	float computeSpillPos(float portion) {
+		var adjustedHealthPortion = Mathf.Clamp01((portion - minHealthPortion) * (1 / (1 - minHealthPortion)));
+		return (minSpillPos * (1 - adjustedHealthPortion)) + (maxSpillPos * adjustedHealthPortion);
 	}
 }

2bf2703ef133f72cfe0dfebe9f04a5f157e2bc6f
Sam Trapp <>
2023-04-29 23:15:46 -0600

    Exposing event on NpcDie

--- Assets/Scripts/NpcDie.cs
+++ Assets/Scripts/NpcDie.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NpcDie : MonoBehaviour {
+	public UnityEvent OnDie;
 	public void Die() {
+		OnDie.Invoke();
 		Destroy(gameObject);
 	}
 }

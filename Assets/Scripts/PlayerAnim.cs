7df73d9d49477e725121db54766f4b67b9608f9c
Sam Trapp <>
2023-05-01 19:34:45 -0600

    fixing THE bug

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -13,7 +13,7 @@ public class PlayerAnim : MonoBehaviour {
 	void Update() {
 		var nextPose = "Idle";
 
-		if (groundCheck.OnGround) {
+		if (groundCheck.OnGround && !playerFeed.IsFeeding) {
 			nextPose = playerMove.GetAnimParam();
 		} else {
 			if (playerFeed != null && playerFeed.IsFeeding) {
